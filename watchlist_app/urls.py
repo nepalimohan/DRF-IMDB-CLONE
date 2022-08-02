52d20a73d63c99f1d9cf7c8dfa5cb1dc1778b1f9
nepalimohan <nepalimohan@outlook.com>
2022-08-02 12:57:36 +0545

    Filesupdated

--- watchlist_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('list/', views.movie_list, name='movie_list'),
-    path('<int:pk>', views.movie_details, name='movie_details'),
-]
--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,19 +1,19 @@
-from django.shortcuts import render
-from .models import Movie
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from .models import Movie
+# from django.http import JsonResponse
 
-def movie_list(request):
-    movies = Movie.objects.all()
-    data = {
-        'movies':list(movies.values())
-    }
-    return JsonResponse(data)
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     data = {
+#         'movies':list(movies.values())
+#     }
+#     return JsonResponse(data)
 
-def movie_details(request, pk):
-    movies = Movie.objects.get(pk=pk)
-    data = {
-        'name': movies.name,
-        'description': movies.description,
-        'active': movies.active,
-    }
-    return JsonResponse(data) 
+# def movie_details(request, pk):
+#     movies = Movie.objects.get(pk=pk)
+#     data = {
+#         'name': movies.name,
+#         'description': movies.description,
+#         'active': movies.active,
+#     }
+#     return JsonResponse(data) 
