2fd77bdc486adf3ab32e0ae16d2ebf83bf654415
nepalimohan <nepalimohan@outlook.com>
2022-08-02 20:02:21 +0545

    POSTimplemented

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,19 +1,26 @@
-from django.shortcuts import render
+from re import A
+import re
+from rest_framework.response import Response
 from watchlist_app.models import Movie
-from django.http import JsonResponse
+from watchlist_app.api.serializers import MovieSerializer
+from rest_framework.decorators import api_view
 
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    data = {
-        'movies':list(movies.values())
-    }
-    return JsonResponse(data)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
 
-def movie_details(request, pk):
-    movies = Movie.objects.get(pk=pk)
-    data = {
-        'name': movies.name,
-        'description': movies.description,
-        'active': movies.active,
-    }
-    return JsonResponse(data) 
+@api_view() 
+def movie_details(request,pk):
+    movies =  Movie.objects.get(pk=pk)
+    serializer = MovieSerializer(movies)
+    return Response(serializer.data)
