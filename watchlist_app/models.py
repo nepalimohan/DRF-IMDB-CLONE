07cc5fb2332085daada6cd1e4628015f604fb11c
nepalimohan <nepalimohan@outlook.com>
2022-08-16 12:06:58 +0545

    ReviewModelCreated

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,6 +1,7 @@
 from unicodedata import name
 from django.db import models
 from django.forms import URLField
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class StreamPlatform(models.Model):
@@ -20,3 +21,14 @@ class Watchlist(models.Model):
     
     def __str__(self):
         return self.title
+    
+class Review(models.Model):
+    rating = models.PositiveBigIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    description = models.CharField(max_length=255, null=True)
+    watchlist = models.ForeignKey(Watchlist, on_delete=models.CASCADE, related_name="reviews")
+    active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.rating) + " | " + self.watchlist.title
