3ff815457ae7e6b52dc5725fbf681d4437bf9553
nepalimohan <nepalimohan@outlook.com>
2022-08-20 17:08:12 +0545

    URLsUpdated

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -9,7 +9,11 @@ urlpatterns = [
     path('stream/', StreamPlatformAV.as_view(), name='stream'), 
     path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream_details'), 
     
-    path('review', ReviewList.as_view(), name= "review-list"),
-    path('review/<int:pk>', ReviewDetails.as_view(), name= "review-details"),
+    # path('review', ReviewList.as_view(), name= "review-list"),
+    # path('review/<int:pk>', ReviewDetails.as_view(), name= "review-details"),
+    
+    
+    path('stream/<int:pk>/review', StreamPlatformDetailAV.as_view(), name='stream_details'), 
+    path('stream/review/<int:pk>', ReviewDetails.as_view(), name= "review-details"),
     
 ]
