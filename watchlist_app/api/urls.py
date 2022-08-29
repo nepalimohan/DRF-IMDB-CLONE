d701583a0069cf66950fc60a27f6e0a967210fab
nepalimohan <nepalimohan@outlook.com>
2022-08-29 09:53:33 +0545

    RatingsAdded

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -10,7 +10,7 @@ router.register('stream', StreamPlatformVS, basename='streamplatform')
 
 urlpatterns = [
     path('list/',  WatchListAV.as_view(), name='watch_list'),
-    path('<int:pk>', WatchDetailsAV.as_view(), name='watch_details'),
+    path('<int:pk>/', WatchDetailsAV.as_view(), name='watch_details'),
     
     # path('stream/', StreamPlatformAV.as_view(), name='stream'), 
     # path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream_details'), 
@@ -21,8 +21,8 @@ urlpatterns = [
     # path('review/<int:pk>', ReviewDetails.as_view(), name= "review-details"),
     
     
-    path('stream/<int:pk>/review-create', ReviewCreate.as_view(), name='review-create'),
-    path('stream/<int:pk>/review', ReviewList.as_view(), name='review-list'),
-    path('stream/review/<int:pk>', ReviewDetails.as_view(), name= "review-details"),
+    path('<int:pk>/review-create/', ReviewCreate.as_view(), name='review-create'),
+    path('<int:pk>/review/', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>/', ReviewDetails.as_view(), name= "review-details"),
     
 ]
