26d81f80ae153004e95c317b0a01200a73111182
nepalimohan <nepalimohan@outlook.com>
2022-09-01 11:21:35 +0545

    RegistrationDone

--- user_app/api/serializers.py
+++ user_app/api/serializers.py
@@ -10,5 +10,22 @@ class RegistrationSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'password2']
         #django already contains username, email and password so we need to define password2
         extra_kwargs = {
-            'password': {'wtrite_only': True}
+            'password': {'write_only': True}
         }
+        
+    #overriding self method to solve extra password field error
+    def save(self):
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+        
+        if password != password2:
+            raise serializers.ValidationError({'Error': 'Passwords do not match'})
+        
+        if User.objects.filter(email= self.validated_data['email']).exists():
+            raise serializers.ValidationError({'Error': 'Email already exits'})
+            
+        account = User(email= self.validated_data['email'], username= self.validated_data['username'])
+        account.set_password(self.validated_data['password'])
+        account.save()
+        
+        return account
